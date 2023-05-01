52fda3b33341472a1e3da3b54cf9e351658bcf96
ShawnDriscoll <shawndriscoll@hotmail.com>
2023-05-01 14:29:46 -0700

    Beta extended

--- pytravcalc.py
+++ pytravcalc.py
@@ -1686,7 +1686,7 @@ if __name__ == '__main__':
     
     if len(sys.argv) < 2:
 
-        if trange[0] > 2023 or trange[1] > 4:
+        if trange[0] > 2023 or trange[1] > 7:
             __expired_tag__ = True
             __app__ += ' [EXPIRED]'
 
@@ -1730,7 +1730,7 @@ if __name__ == '__main__':
 
         app.exec_()
     
-    elif trange[0] > 2023 or trange[1] > 4:
+    elif trange[0] > 2023 or trange[1] > 7:
         __app__ += ' [EXPIRED]'
         '''
         Beta for this app has expired!
